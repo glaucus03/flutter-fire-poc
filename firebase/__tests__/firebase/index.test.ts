82d59069b082376ebda4f9f3c94a46600438b3f3
glaucus03 <glaucus03.tech@gmail.com>
2023-12-04 23:58:40 +0900

    wip: add comment todo :construction:

--- firebase/__tests__/firebase/index.test.ts
+++ firebase/__tests__/firebase/index.test.ts
@@ -5,3 +5,32 @@ describe("init test", ()=> {
     expect(1).toBe(1);
   })
 })
+// TODO: 
+  // describe("test", () => {
+  //   beforeAll(async () => {
+  //     await initializeTestEnvironment();
+  //     env = getTestEnv();
+  //   });
+  //
+  //   afterAll(async () => {
+  //     await env.cleanup();
+  //   });
+  //
+  //   beforeEach(async () => {
+  //     await env.withSecurityRulesDisabled(async context => {
+  //       const adminDb = context.firestore()
+  //       await adminDb.collection("users").doc(users.id).set(users.data);
+  //       await adminDb.collection("users").doc(other.id).set(other.data);
+  //     })
+  //     updateUsersData = Object.assign({}, users.data, {name: 'update-users'})
+  //     updateOtherData = Object.assign({}, users.data, {name: 'update-other'})
+  //   })
+  //   afterEach(async () => {
+  //     await env.clearFirestore();
+  //   });
+  //   test("test", async () => {
+  //     const authenticatedDb = getFirestore(env.unauthenticatedContext());
+  //     const ref = authenticatedDb.collection("users").doc(users.id);
+  //     await assertFails(ref.update(updateUsersData));
+  //   });
+
